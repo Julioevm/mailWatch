8ce7e397ed1e0a27837d3429cf586e5ef851b79c
Julio <julioevm@gmail.com>
2019-05-05 21:19:08 +0200

    Made some small changes

--- mailWatch.py
+++ mailWatch.py
@@ -7,9 +7,16 @@ REFRESH = 300
 CREDENTIALS = ()
 ACCOUNT = ()
 
-READC = '\33[34m'
-UNREADC = '\033[0m'
+colors = {
+
+        'blue': '\33[34m',
+        'white': '\033[0m'
+        }
 TIME_FORMAT = '%d/%m/%Y %H:%M'
+
+def colorize(string, color):
+    if not color in colors: return string
+    return colors[color] + string + '\033[0m'
 
 def loadConfig():
     
@@ -45,23 +52,30 @@ def setAccount(USER, PASSWORD):
         access_type=DELEGATE
     )
 
+def get_mail(inbox,num):
+
+    return inbox.all().order_by('-datetime_received')[:num]
+
 def main():
     
     loadConfig()
     setAccount(USER, PASSWORD)
-
     inbox = ACCOUNT.root / 'Top of Information Store' / 'Inbox'
-
     print('Unread email:' + str(inbox.unread_count))
 
     print('Last 10 emails received:')
-    for item in inbox.all().order_by('-datetime_received')[:10]:
-        if item.is_read:
-            color = READC
+
+    mails = get_mail(inbox, 10)
+    i = 1
+    for mail in mails:
+        line = '['+str(i)+']' + ' ' + mail.datetime_received.strftime(TIME_FORMAT)+ ' ' + mail.subject + ' - ' + mail.sender.name 
+        if mail.is_read:
+            color = 'blue'
         else:
-            color = UNREADC
+            color = 'white'
         
-        print(color, item.subject, item.sender.name, item.datetime_received.strftime(TIME_FORMAT))
+        print(colorize(line, color))
+        i += 1
 
-
-main()
+if __name__ == "__main__":
+    main()
