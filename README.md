29d9ae4816e08a9c64de267e33dc0f430b4da757
Julio <julio.vallsmartinez@amaris.com>
2019-06-10 16:44:26 +0200

    Add functionality to open individual mails

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # mailWatch
 Python program to check Microsoft exchange mail.
+## Dependencies
+Exchangelib, html2text and getkey are necessary to run this program.
+```
+pip install exchangelib html2text getkey
+```
 ## Configuration
 A config file named **config.ini** is necessary for this program to work with the following contents:
 ```
@@ -13,7 +18,7 @@ For now the config must be on the same folder as the script.
 
 Future features include:
 
-* Daemon mode: simply run on the background and  notify of new email
-* Open email items to read them
+* Daemon mode: simply run on the background and notify of new email
+* Open email items to read them :heavy_check_mark:
 * Multiple pages of mail
 * Better UI
--- mailWatch.py
+++ mailWatch.py
@@ -1,25 +1,34 @@
 from exchangelib import DELEGATE, Account, Credentials, Folder
+from getkey import getkey, keys
+import html2text
 import configparser
+
 
 USER = ''
 PASSWORD = ''
 REFRESH = 300
 CREDENTIALS = ()
 ACCOUNT = ()
+INBOX = ()
+MAILS = []
+RUN = True
 
 colors = {
 
-        'blue': '\33[34m',
-        'white': '\033[0m'
-        }
+    'blue': '\33[34m',
+    'white': '\033[0m'
+}
 TIME_FORMAT = '%d/%m/%Y %H:%M'
 
+
 def colorize(string, color):
-    if not color in colors: return string
+    if not color in colors:
+        return string
     return colors[color] + string + '\033[0m'
 
+
 def loadConfig():
-    
+
     global USER
     global PASSWORD
     global REFRESH
@@ -47,35 +56,83 @@ def setAccount(USER, PASSWORD):
 
     ACCOUNT = Account(
         primary_smtp_address=USER,
-        credentials=CREDENTIALS, 
-        autodiscover=True, 
+        credentials=CREDENTIALS,
+        autodiscover=True,
         access_type=DELEGATE
     )
 
-def get_mail(inbox,num):
+def get_mail(inbox, num):
 
-    return inbox.all().order_by('-datetime_received')[:num]
+    return list(inbox.all().order_by('-datetime_received')[:num])
 
-def main():
-    
-    loadConfig()
-    setAccount(USER, PASSWORD)
-    inbox = ACCOUNT.root / 'Top of Information Store' / 'Inbox'
-    print('Unread email:' + str(inbox.unread_count))
 
-    print('Last 10 emails received:')
-
-    mails = get_mail(inbox, 10)
-    i = 1
+def show_mail_list(mails):
+    i = 0
     for mail in mails:
-        line = '['+str(i)+']' + ' ' + mail.datetime_received.strftime(TIME_FORMAT)+ ' ' + mail.subject + ' - ' + mail.sender.name 
+        line = '['+str(i)+']' + ' ' + mail.datetime_received.strftime(
+            TIME_FORMAT) + ' ' + mail.subject + ' - ' + mail.sender.name
         if mail.is_read:
             color = 'blue'
         else:
             color = 'white'
-        
+
         print(colorize(line, color))
         i += 1
+
+def show_mail(n):
+    global MAILS
+    mail = list(MAILS)[n]
+    print(mail.subject)
+    print(mail.datetime_received.strftime(TIME_FORMAT))
+    print(mail.sender.name)
+    # cleantext = BeautifulSoup(mail.body, "lxml").get_text()
+    print(html2text.html2text(mail.body))
+    input("Press Enter to continue...")
+
+def read_key():
+    global RUN
+    global INBOX
+    global MAILS
+    key = getkey()
+
+    if key == 'r' or key == 'R':
+        print("Refreshing list.")
+        MAILS = get_mail(INBOX, 10)
+    elif key == 'q' or key == 'Q':
+        print("Closing program. Good Bye!")
+        RUN = False
+    else:
+        # Check for integer to select a mail
+        try:
+            key = int(key)
+            if key >= 0 and key <= 9:
+                print("Selecting item ", key)
+                show_mail(key)
+        except:
+           print("Please enter a valid key.")
+
+
+def main():
+
+    global RUN
+    global MAILS
+    global INBOX
+
+    loadConfig()
+    setAccount(USER, PASSWORD)
+    INBOX = ACCOUNT.root / 'Top of Information Store' / 'Inbox'
+    MAILS = get_mail(INBOX, 10)
+
+    while RUN:
+        print('Unread email:' + str(INBOX.unread_count))
+
+        print('Last 10 emails received:')
+
+        show_mail_list(MAILS)
+
+        print("Enter the number of item to open, R to refresh list.")
+        read_key()
+
 
 if __name__ == "__main__":
     main()
